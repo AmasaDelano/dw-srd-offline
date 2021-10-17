b3f3e5a4fdf07d89596c596a180890b0ed4bf26a
Alex Leone <me@alexjleone.com>
2021-10-16 23:36:05 -0500

    Simplified to try to get PWA to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use script";
 
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/dw-srd-offline/sw.js", {scope: "/dw-srd-offline/"})
         .then(() => { console.log("Service Worker Registered"); });
