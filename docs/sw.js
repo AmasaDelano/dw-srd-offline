5958d4566d3151074f6e379aba19b0b9ecf7e0b8
Alex Leone <me@alexjleone.com>
2021-10-16 23:31:20 -0500

    Updated service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,19 +1,62 @@
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open("dw-store").then((cache) => cache.addAll([
-            "/dw-srd-offline/",
-            "/dw-srd-offline/index.html",
-            "/dw-srd-offline/bundle.js",
-            "/dw-srd-offline/style.css",
-            "/dw-srd-offline/logo.png",
-            ])),
-        );
-    });
+// https://gist.github.com/kosamari/7c5d1e8449b2fbc97d372675f16b566e
 
-    self.addEventListener("fetch", (e) => {
-    console.log(e.request.url);
+var APP_PREFIX = "DWSRDOFFLINE_";     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = "01";            // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [                            
+    "/dw-srd-offline/",
+    "/dw-srd-offline/index.html",
+    "/dw-srd-offline/bundle.js",
+    "/dw-srd-offline/style.css",
+    "/dw-srd-offline/logo.png"
+];
 
+self.addEventListener("fetch", function (e) {
+    console.log("fetch request : " + e.request.url)
     e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log("responding with cache : " + e.request.url);
+                return request;
+            } else {       // if there are no cache, try fetching request
+                console.log("file is not cached, fetching : " + e.request.url);
+                return fetch(e.request);
+            }
+    
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    );
+});
+  
+  // Cache resources
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("installing cache : " + CACHE_NAME);
+            return cache.addAll(URLS);
+        })
+    );
+});
+  
+// Delete outdated caches
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create white list
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            // add current cache name to white list
+            cacheWhitelist.push(CACHE_NAME);
+    
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log("deleting cache : " + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
     );
 });
